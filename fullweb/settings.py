5826d0c541c9815155f88a65cbd459792d534a2f
maricm123 <mihailomaric001@gmail.com>
2020-11-06 10:34:36 +0100

    Ubacen shop, samo osnove

--- fullweb/settings.py
+++ fullweb/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
+    'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -63,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -129,3 +134,10 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mihailomaric001@gmail.com'
 EMAIL_HOST_PASSWORD = 'MARIC001'
 EMAIL_USE_TLS = True
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
