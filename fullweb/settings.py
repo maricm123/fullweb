fa9d0a8870421ad4357fcf44a807c1b6054654cb
maricm123 <48715964+maricm123@users.noreply.github.com>
2022-03-09 15:31:20 +0100

    Update settings.py

--- fullweb/settings.py
+++ fullweb/settings.py
@@ -129,11 +129,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://myaccount.google.com/apppaswords
 # https://accounts.google.com/DisplayUnclockCaptcha
 #email settings
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mihailomaric001@gmail.com'
-EMAIL_HOST_PASSWORD = 'MARIC001'
-EMAIL_USE_TLS = True
+
 
 
 
